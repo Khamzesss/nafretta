a6a779fcf51a3300f4cd9b7af31b87718c672ff6
Khamzesss <zhamzat00700795@gmail.com>
2024-10-25 11:40:03 +0300

    Update next.js

--- next.js
+++ next.js
@@ -4,14 +4,6 @@ var randomNumber = Math.floor(Math.random() * 10001);
 // Вывод случайного числа на страницу
 document.getElementById("randomNumber").innerText =
   "Ваш уникальный код: " + randomNumber;
-function copyNumber() {
-  var number = "+79280001810";
-  navigator.clipboard
-    .writeText(number)
-    .then(() => {
-      alert("Номер скопирован: " + number);
-    })
-    .catch((err) => {
-      console.error("Failed to copy: ", err);
-    });
-}
+
+
+
