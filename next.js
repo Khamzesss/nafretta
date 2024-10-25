8a8199cfad04086e427ffa7dc66ccd86b9937b85
Khamzesss <zhamzat00700795@gmail.com>
2024-10-25 11:41:18 +0300

    Update next.js

--- next.js
+++ next.js
@@ -4,6 +4,3 @@ var randomNumber = Math.floor(Math.random() * 10001);
 // Вывод случайного числа на страницу
 document.getElementById("randomNumber").innerText =
   "Ваш уникальный код: " + randomNumber;
-
-
-
